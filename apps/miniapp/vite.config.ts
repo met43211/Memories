ad6d946b4e67815f0dd026caf961167bd222754a
met43211 <matvey-anashkin@yandex.ru>
2026-07-12 16:46:29 +0300

    fix: lint errors

--- apps/miniapp/vite.config.ts
+++ apps/miniapp/vite.config.ts
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    target: "es2022",
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          groups: [
-            {
-              name: "react",
-              test: (id) =>
-                /node_modules[\\/](react-dom|react)/.test(id),
-            },
-          ],
-        },
-      },
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	build: {
+		target: "es2022",
+		rolldownOptions: {
+			output: {
+				codeSplitting: {
+					groups: [
+						{
+							name: "react",
+							test: (id) => /node_modules[\\/](react-dom|react)/.test(id),
+						},
+					],
+				},
+			},
+		},
+	},
+});
